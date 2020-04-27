f78267e7439a912c197945a105651c75ad9c461c
Serhii Chernov <chernovserhiii@gmail.com>
2020-04-27 09:02:43 +0200

    Add project files.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace LibrarySystemManagement
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+   
+    public partial class MainWindow : Window
+    {
+        SqlConnection c = new SqlConnection(@"Data Source=DESKTOP-SOMLP49;Initial Catalog=LibraryManagementDB;Integrated Security=True;Pooling=False");
+        int count = 0;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            SqlCommand cmd = c.CreateCommand();
+            cmd.CommandType = CommandType.Text;
+            cmd.CommandText = "select * from Library_Employer where UserName='"+textbox1.Text+"'and Password='"+textbox2.Text+"'";
+            cmd.ExecuteNonQuery();
+            DataTable dt = new DataTable();
+            SqlDataAdapter sda = new SqlDataAdapter(cmd);
+            sda.Fill(dt);
+            count = Convert.ToInt32(dt.Rows.Count.ToString());
+            if (count == 0)
+            {
+                MessageBox.Show("Error: You need to write your Username and password to login into Library");
+            }
+            else
+            {
+                this.Hide();
+                UserWindow uw = new UserWindow();
+                uw.Show();
+            }
+        }
+
+      
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            if (c.State == ConnectionState.Open)
+            {
+                c.Close();
+            }
+            c.Open();
+        }
+    }
+}
